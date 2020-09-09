253e8416054c9c69966d5b42f2ef510b211745cf
Bianca Ferreira <biancaferreiralp@gmail.com>
2020-09-09 13:43:24 -0300

    Updated edit form and linked to navbar, created a new users controller, added show action/view and also linked to navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+biancaferreira = User.create!(first_name: 'Bianca', last_name: 'Ferreira', document: '123456789', phone_number: '00000000', 
+                          address: 'Rua Retiro dos Artistas', email: 'biancaferreiralp@gmail.com', 
+                          password: '123456');

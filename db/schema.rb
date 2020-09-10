810993a9230b718014e081fbc19cfd5faad54244
Berenice Mahe <berenice.mahe@gmail.com>
2020-09-10 17:33:44 -0300

    managing seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_181634) do
+ActiveRecord::Schema.define(version: 2020_09_10_194348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,6 @@ ActiveRecord::Schema.define(version: 2020_09_10_181634) do
     t.string "day"
     t.string "hour"
     t.string "price"
-    t.integer "students_number"
-    t.string "speciality"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["teacher_id"], name: "index_bookings_on_teacher_id"

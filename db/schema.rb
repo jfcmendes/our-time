bc0ba454fc928f5b60c14d2ab685c2a700d6b15d
Berenice Mahe <berenice.mahe@gmail.com>
2020-09-10 14:15:11 -0300

    adding diploma to teacher in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_202535) do
+ActiveRecord::Schema.define(version: 2020_09_10_171117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_202535) do
     t.string "max_distance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "diploma"
     t.index ["user_id"], name: "index_teachers_on_user_id"
   end
 

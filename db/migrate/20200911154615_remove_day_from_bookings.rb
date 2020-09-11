9d4f97562f76b43282beec4dca688f1907bf17e5
Bianca Ferreira <biancaferreiralp@gmail.com>
2020-09-11 12:48:44 -0300

    Generated migrations to Bookings table - added date and removed day and hour

---
+++ db/migrate/20200911154615_remove_day_from_bookings.rb
@@ -0,0 +1,5 @@
+class RemoveDayFromBookings < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :bookings, :day
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_194348) do
+ActiveRecord::Schema.define(version: 2020_09_11_154718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,11 +44,10 @@ ActiveRecord::Schema.define(version: 2020_09_10_194348) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "teacher_id", null: false
     t.bigint "user_id", null: false
-    t.string "day"
-    t.string "hour"
     t.string "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "date"
     t.index ["teacher_id"], name: "index_bookings_on_teacher_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end

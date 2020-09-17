8220480e001244fdc95f01164290c520ba274cb9
Berenice Mahe <berenice.mahe@gmail.com>
2020-09-17 16:40:24 -0300

    chatrooms views fixed

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,15 +1,17 @@
 import consumer from "./consumer";
 
 const initChatroomCable = () => {
-  const messagesContainer = document.getElementById('messages');
-  if (messagesContainer) {
-    const id = messagesContainer.dataset.chatroomId;
+  const messagesContainers = document.querySelectorAll('.messages');
+  if (messagesContainers.length > 0) {
+    messagesContainers.forEach((messagesContainer)=>{
+      const id = messagesContainer.dataset.chatroomId;
 
-    consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-      received(data) {
-        messagesContainer.insertAdjacentHTML("beforeend", data) // called when data is broadcast in the cable
-      },
-    });
+      consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
+        received(data) {
+          messagesContainer.insertAdjacentHTML("beforeend", data) // called when data is broadcast in the cable
+        },
+      });
+    })
   }
 }
 
